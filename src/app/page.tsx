c57b4f698e7853f7388b9b5e016ae7e58abdbfbe
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-03 18:33:56 +0330

    Added some todos to main page to do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const page = () => {
-
-    // TODO: Add a functionality to `add a task` button
+    /*
+        TODO: Add a functionality to "Add a task" button
+        TODO: Create "Delete task" button
+        TODO: Create "Mark as completed" button
+    */
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center">
@@ -13,7 +16,7 @@ const page = () => {
                     className="border-none outline-none rounded-l-full px-10 py-3 transition-all duration-300 ease-in-out dark:border-white"
                 />
                 <button className="rounded-r-full bg-sky-400 p-3 font-bold text-white">
-                    Add task
+                    Add a task
                 </button>
             </div>
         </div>
