e3287b47784b98879952fc1c0b8850e1d084525e
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-08 19:52:09 +0330

    Added local settings for vscode, added some todos to do and moved TodosList.tsx content to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 const page = () => {
     /*
-        TODO: Add a functionality to "Add a task" button
         TODO: Create "Delete task" button
-        TODO: Create "Mark as completed" button
+        TODO: Create "Mark as completed" button or when clicked a todo, set it to completed
     */
+
+    const [todos, setTodos] = useState<string[]>([]);
+    const [completedTodos, setCompletedTodos] = useState<string[]>([]);
+    const [todoname, setTodoname] = useState<string>();
+
+    const addTodo = (todo: string) => {
+        setTodos([...todos, todo]);
+    };
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center">
             <h1 className="text-6xl font-bold">Todo List</h1>
-            <div className="mt-10 flex items-center justify-center transition-all duration-300 ease-in-out hover:shadow-2xl">
+            <section className="mt-10 flex items-center justify-center transition-all duration-300 ease-in-out hover:shadow-2xl">
                 <input
                     type="text"
-                    className="border-none outline-none rounded-l-full px-10 py-3 transition-all duration-300 ease-in-out dark:border-white"
+                    className="rounded-l-full border-none px-10 py-3 text-2xl outline-none transition-all duration-300 ease-in-out dark:border-white"
+                    value={todoname}
                 />
-                <button className="rounded-r-full bg-sky-400 p-3 font-bold text-white">
+                <button
+                    className="rounded-r-full bg-sky-400 p-3 font-bold text-white"
+                    onClick={() => addTodo("Add a task")}
+                >
                     Add a task
                 </button>
-            </div>
+            </section>
         </div>
     );
 };
