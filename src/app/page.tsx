0e285f41434e1730c271d86370689d695daa0581
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-11 12:23:01 +0330

    Added new styles to todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Todo from "../lib/components/Todo";
 
 const page = () => {
     /*
@@ -11,8 +12,12 @@ const page = () => {
     const [completedTodos, setCompletedTodos] = useState<string[]>([]);
     const [todoname, setTodoname] = useState<string>();
 
-    const addTodo = (todo: string) => {
-        setTodos([...todos, todo]);
+    const addTodo = () => {
+        if (todoname) {
+            // Update the todos state with the new todo
+            setTodos([...todos, todoname]);
+            setTodoname(""); // Clear the input field after adding
+        }
     };
 
     return (
@@ -22,15 +27,21 @@ const page = () => {
                 <input
                     type="text"
                     className="rounded-l-full border-none px-10 py-3 text-2xl outline-none transition-all duration-300 ease-in-out dark:border-white"
-                    value={todoname}
+                    value={todoname || ""} // Ensure value is empty if todoname is null
+                    onChange={(e) => setTodoname(e.target.value)} // Update todoname on input change
                 />
                 <button
                     className="rounded-r-full bg-sky-400 p-3 font-bold text-white"
-                    onClick={() => addTodo("Add a task")}
+                    onClick={addTodo} // Call addTodo function on click
                 >
-                    Add a task
+                    Add
                 </button>
             </section>
+            <ul className="*:text-3xl *:text-left *:hover:line-through">
+                {todos.map((todo) => (
+                    <Todo key={todo}>{todo}</Todo>
+                ))}
+            </ul>
         </div>
     );
 };
