dff151a1bc759b6277199fb040f3cabae06fe41e
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-11 12:21:01 +0330

    Added new styles

--- src/lib/components/Todo.tsx
+++ src/lib/components/Todo.tsx
@@ -8,9 +8,9 @@ const Todo: React.FC<TodoProps> = ({ children }) => {
     return (
         <li
             key={Math.random().toString(36).substring(2, 15)}
-            className="mt-5 rounded-md p-2 text-xl transition-all duration-300 ease-in-out hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
+            className="mt-10 rounded-md p-2 align-middle text-xl transition-all duration-300 ease-in-out *:flex *:flex-row hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
         >
-            &#xf111; {children}
+            {children}
         </li>
     );
 };
