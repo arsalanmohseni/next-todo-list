75457bca02c9c02a6b129c2ceebfd886157c92f9
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-11 11:52:21 +0330

    Formated and lited Todo.tsx

--- src/lib/components/Todo.tsx
+++ src/lib/components/Todo.tsx
@@ -8,9 +8,9 @@ const Todo: React.FC<TodoProps> = ({ children }) => {
     return (
         <li
             key={Math.random().toString(36).substring(2, 15)}
-            className="rounded-md p-2 text-lg transition-all duration-300 ease-in-out hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
+            className="mt-5 rounded-md p-2 text-xl transition-all duration-300 ease-in-out hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
         >
-            {children}
+            &#xf111; {children}
         </li>
     );
 };
