ea0f2125e6082298bd33664f3e470ad4403945ef
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-10-11 11:41:35 +0330

    Added some new styles to all of todos

--- src/lib/components/Todo.tsx
+++ src/lib/components/Todo.tsx
@@ -6,7 +6,12 @@ interface TodoProps {
 }
 const Todo: React.FC<TodoProps> = ({ children }) => {
     return (
-        <li key={Math.random().toString(36).substring(2, 15)}>{children}</li>
+        <li
+            key={Math.random().toString(36).substring(2, 15)}
+            className="rounded-md p-2 text-lg transition-all duration-300 ease-in-out hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
+        >
+            {children}
+        </li>
     );
 };
 
